5847060d63b7b70515307f0162d21509a6c9bfa8
Sandie3 <sandermunkh@gmail.com>
2022-03-04 12:51:16 +0100

    Creating authentication
    Bunch of fucking errors

--- src/Components/Layout/LoginBox.jsx
+++ src/Components/Layout/LoginBox.jsx
@@ -1,65 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import { login } from '../../Components/Helpers/Login'
+import {
+	BrowserRouter,
+	Route,
+	Routes,
+	Link,
+	useNavigate,
+	useLocation,
+	Navigate,
+	Outlet
+} from 'react-router-dom'
+import * as Login from '../../Components/Helpers/Login'
 
 export const LoginBox = () => {
 
-	const handleLogin = ( e ) => {
+	let navigate = useNavigate();
+	let location = useLocation();
+
+	let from = location.pathname || "/";
+
+	function handleSubmit ( e ) {
 		e.preventDefault();
-		login( e.target )
-			.then( res => {
-				console.log( res )
-				IsLoggedIn( res )
-			} )
-	}
-
-	// useEffect(() => {
-		
-	// 	IsLoggedIn(false)
-
-	// }, [])
-	
-
-	const IsLoggedIn = ( e ) => {
-		if ( e.login == false ) {
-			console.log( 'Not logged in' )
-			localStorage.setItem('loggedIn', false);
-		} else {
-			console.log( 'Logged in' )
-			localStorage.setItem('loggedIn', true);
-		}
+		Login.login( e.currentTarget ).then( res => {
+			navigate( from, { replace: true } );
+		} )
 	}
 
 	return (
-		<section>
-			<div className="wrapper">
-				<div className="colWrap">
-					<div className='bgCol'></div>
-					<div className='col2'></div>
-					<div className='col3'></div>
-					<div className='col4'></div>
-					<div className='col5'></div>
-					<div className='col6'></div>
-					<div className='col7'></div>
-					<div className='col8'></div>
-					<div className='col9'></div>
-				</div>
-				<form className="loginForm" onSubmit={ handleLogin }>
-					<h1>Login</h1>
-					<div className="formWrapper">
-						<div className="txtBox">
-							<input type="text" id="email" name="email" placeholder='email@example.com' />
-						</div>
-						<div className="txtBox">
-							<input type="password" id="password" name="password" placeholder='******' />
-						</div>
-						<input type="submit" className="logBtn" value="Login" />
-						<div className="bottomText">
-							<span>Don't have account?</span>
-							<a href="#">Sign up now</a>
-						</div>
+		<>
+			<p>You must log in to view the page at { from }</p>
+			<section>
+				<div className="wrapper">
+					<div className="colWrap">
+						<div className='bgCol'></div>
+						<div className='col2'></div>
+						<div className='col3'></div>
+						<div className='col4'></div>
+						<div className='col5'></div>
+						<div className='col6'></div>
+						<div className='col7'></div>
+						<div className='col8'></div>
+						<div className='col9'></div>
 					</div>
-				</form>
-			</div >
-		</section >
+					<form className="loginForm" onSubmit={ handleSubmit }>
+						<h1>Login</h1>
+						<div className="formWrapper">
+							<div className="txtBox">
+								<input type="text" id="email" name="email" placeholder='email@example.com' />
+							</div>
+							<div className="txtBox">
+								<input type="password" id="password" name="password" placeholder='******' />
+							</div>
+							<input type="submit" className="logBtn" value="Login" />
+							<div className="bottomText">
+								<span>Don't have account?</span>
+								<a href="#">Sign up now</a>
+							</div>
+						</div>
+					</form>
+				</div >
+			</section >
+		</>
 	)
 }
--- src/Components/Layout/Nav.jsx
+++ src/Components/Layout/Nav.jsx
@@ -3,46 +3,34 @@ import { Outlet, Link } from "react-router-dom";
 
 import { logout } from '../Helpers/Login'
 
+import { AuthStatus } from '../Helpers/Auth.tsx';
+
 const Nav = () => {
 
-	const handleLogout = ( e ) => {
-		e.preventDefault();
-		logout()
-		.then( res => {
-			console.log( res )
-			IsLoggedIn( res )
-		} )
-	}
-	
-	let loggedIn = localStorage.getItem('loggedIn');
-	useEffect(() => {
-
-		if (loggedIn == null || loggedIn == false) {
-			loggedIn = false;
-		}
-		
-		IsLoggedIn(loggedIn)
-
-	}, [])
-	
-
-	const IsLoggedIn = ( e ) => {
-		if ( e.login == false || e == false || e == null ) {
-			console.log( 'Not logged in' )
-			localStorage.setItem('loggedIn', false);
-		} else {
-			console.log( 'Logged in' )
-			localStorage.setItem('loggedIn', true);
-		}
-	}
+	// const handleLogout = ( e ) => {
+	// 	e.preventDefault();
+	// 	logout()
+	// 	.then( res => {
+	// 		console.log( res )
+	// 		IsLoggedIn( res )
+	// 	} )
+	// }
 
 	return (
-		<>
+		<div>
 			<nav>
-				<a href="#" onClick={ handleLogout }>Logout</a>
+				<Link to="/">Public Page</Link>
+				<Link to="/protected">Protected Page</Link>
 			</nav>
+			<AuthStatus />
 			<Outlet />
-		</>
+		</div>
+		// <>
+		// 	<nav>
+		// 		<a href="#" onClick={ handleLogout }>Logout</a>
+		// 	</nav>
+		// 	<Outlet />
+		// </>
 	)
 }
 
